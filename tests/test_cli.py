582b5e62759f071524d003fba041e163a08ca0fc
Samuel Colvin <s@muelcolvin.com>
2016-02-09 19:42:57 +0000

    Fix test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,7 +7,7 @@ def test_cli_help():
     runner = CliRunner()
     result = runner.invoke(cli, ['--help'])
     assert result.exit_code == 0
-    assert 'harrier - Jinja2 & sass/scss aware site builder builder' in result.output
+    assert 'harrier - Jinja2 & sass/scss aware site builder' in result.output
 
 
 def test_simple_build(tmpworkdir):
